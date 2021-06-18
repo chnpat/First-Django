7a7959b6c5475459b9042e309195201dee5e7e38
chnpat <charnon.top@hotmail.com>
2021-06-18 16:17:33 +0900

    - feat: add a voting application to git

---
+++ polls/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+# Create your models here.
+
+# Question
+# --------
+# question_text
+# publish_date
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    publish_date = models.DateTimeField('Date Published')
+
+    def __str__(self):
+        return self.question_text
+
+
+# Choice
+# --------
+# choice_text
+# number_of_votes
+# link
+
+class Choice(models.Model):
+    choice_text = models.CharField(max_length=200)
+    vote = models.IntegerField(default=0)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.choice_text
